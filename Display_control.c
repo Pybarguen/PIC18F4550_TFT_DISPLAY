805f90460e02e60dfb528f7a922110807e7ef547
Pybarguen <pybarguen@gmail.com>
2023-12-04 19:57:41 -0500

    Feat: Inicio del proyecto para representar datos en pantalla TFT Se ha añadido letras correspondientes al codigo Ascii, se ha mejorado la velocidad del modulo SPI, se ha añadido la funcion de pintar pantalla y añadir letra

---
+++ Display_control.c
@@ -0,0 +1,129 @@
+// CONFIG1L
+#pragma config PLLDIV = 1       // PLL Prescaler Selection bits (No prescale (4 MHz oscillator input drives PLL directly))
+#pragma config CPUDIV = OSC1_PLL2// System Clock Postscaler Selection bits ([Primary Oscillator Src: /1][96 MHz PLL Src: /2])
+#pragma config USBDIV = 2       // USB Clock Selection bit (used in Full-Speed USB mode only; UCFG:FSEN = 1) (USB clock source comes from the 96 MHz PLL divided by 2)
+
+// CONFIG1H
+#pragma config FOSC = XTPLL_XT  // Oscillator Selection bits (XT oscillator, PLL enabled (XTPLL))
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enable bit (Fail-Safe Clock Monitor disabled)
+#pragma config IESO = OFF       // Internal/External Oscillator Switchover bit (Oscillator Switchover mode disabled)
+
+// CONFIG2L
+#pragma config PWRT = OFF       // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOR = OFF        // Brown-out Reset Enable bits (Brown-out Reset disabled in hardware and software)
+#pragma config BORV = 3         // Brown-out Reset Voltage bits (Minimum setting 2.05V)
+#pragma config VREGEN = OFF     // USB Voltage Regulator Enable bit (USB voltage regulator disabled)
+
+// CONFIG2H
+#pragma config WDT = OFF        // Watchdog Timer Enable bit (WDT disabled (control is placed on the SWDTEN bit))
+#pragma config WDTPS = 1        // Watchdog Timer Postscale Select bits (1:1)
+
+// CONFIG3H
+#pragma config CCP2MX = OFF     // CCP2 MUX bit (CCP2 input/output is multiplexed with RB3)
+#pragma config PBADEN = OFF     // PORTB A/D Enable bit (PORTB<4:0> pins are configured as digital I/O on Reset)
+#pragma config LPT1OSC = OFF    // Low-Power Timer 1 Oscillator Enable bit (Timer1 configured for higher power operation)
+#pragma config MCLRE = ON       // MCLR Pin Enable bit (MCLR pin enabled; RE3 input pin disabled)
+
+// CONFIG4L
+#pragma config STVREN = OFF     // Stack Full/Underflow Reset Enable bit (Stack full/underflow will not cause Reset)
+#pragma config LVP = OFF        // Single-Supply ICSP Enable bit (Single-Supply ICSP disabled)
+#pragma config ICPRT = OFF      // Dedicated In-Circuit Debug/Programming Port (ICPORT) Enable bit (ICPORT disabled)
+#pragma config XINST = OFF      // Extended Instruction Set Enable bit (Instruction set extension and Indexed Addressing mode disabled (Legacy mode))
+
+// CONFIG5L
+#pragma config CP0 = OFF        // Code Protection bit (Block 0 (000800-001FFFh) is not code-protected)
+#pragma config CP1 = OFF        // Code Protection bit (Block 1 (002000-003FFFh) is not code-protected)
+#pragma config CP2 = OFF        // Code Protection bit (Block 2 (004000-005FFFh) is not code-protected)
+#pragma config CP3 = OFF        // Code Protection bit (Block 3 (006000-007FFFh) is not code-protected)
+
+// CONFIG5H
+#pragma config CPB = OFF        // Boot Block Code Protection bit (Boot block (000000-0007FFh) is not code-protected)
+#pragma config CPD = OFF        // Data EEPROM Code Protection bit (Data EEPROM is not code-protected)
+
+// CONFIG6L
+#pragma config WRT0 = OFF       // Write Protection bit (Block 0 (000800-001FFFh) is not write-protected)
+#pragma config WRT1 = OFF       // Write Protection bit (Block 1 (002000-003FFFh) is not write-protected)
+#pragma config WRT2 = OFF       // Write Protection bit (Block 2 (004000-005FFFh) is not write-protected)
+#pragma config WRT3 = OFF       // Write Protection bit (Block 3 (006000-007FFFh) is not write-protected)
+
+// CONFIG6H
+#pragma config WRTC = OFF       // Configuration Register Write Protection bit (Configuration registers (300000-3000FFh) are not write-protected)
+#pragma config WRTB = OFF       // Boot Block Write Protection bit (Boot block (000000-0007FFh) is not write-protected)
+#pragma config WRTD = OFF       // Data EEPROM Write Protection bit (Data EEPROM is not write-protected)
+
+// CONFIG7L
+#pragma config EBTR0 = OFF      // Table Read Protection bit (Block 0 (000800-001FFFh) is not protected from table reads executed in other blocks)
+#pragma config EBTR1 = OFF      // Table Read Protection bit (Block 1 (002000-003FFFh) is not protected from table reads executed in other blocks)
+#pragma config EBTR2 = OFF      // Table Read Protection bit (Block 2 (004000-005FFFh) is not protected from table reads executed in other blocks)
+#pragma config EBTR3 = OFF      // Table Read Protection bit (Block 3 (006000-007FFFh) is not protected from table reads executed in other blocks)
+
+// CONFIG7H
+#pragma config EBTRB = OFF      // Boot Block Table Read Protection bit (Boot block (000000-0007FFh) is not protected from table reads executed in other blocks)
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+
+#define CCS LATBbits.LATB2
+#define DCs  LATBbits.LATB3
+#define RES LATBbits.LATB4
+#include <xc.h>
+#define _XTAL_FREQ 48000000
+#include "Fonts.h"
+#include "Pic18f4550_spi.h"
+#include "ST7735.h"
+//#include "Image.h"
+int color_l;
+int color_h;
+//int *const test[] = {
+void main(void) 
+{
+    
+    
+    
+    
+    
+    //SPI
+   Spi_init();//start spi interface
+   Spi_mode(CPOL_1_CPHA_0);//SPI mode 0 0 
+   Spi_clock_mode(SPI_MASTER_CLOCK_DIV_4);//SPI clock = FOSC/4 
+   TRISBbits.RB2 = 0;//SLAVE control PIN
+   TRISBbits.RB3 = 0;//output DC comman/data ST7735 control
+   TRISBbits.RB4 = 0;//output DC comman/data ST7735 control
+   
+
+   
+   __delay_ms(10);
+    RES = 1;
+    CCS = 1;
+    DCs = 0;
+   
+    ST7735S_80_x_160_init();
+   
+      ST7735S_Fill_display(Black_Color); 
+       __delay_ms(1000);
+      
+              ST7735S_Print_Char(Red_Color, '%');
+    //ST7735S_Fill_image(Image_array);
+      
+    
+      
+    
+     
+   
+
+
+
+   
+   while(1)
+   {
+       
+       
+       
+       
+   }    
+    
+     
+
+       
+
+}
---
+++ ST7735.h
@@ -0,0 +1,373 @@
+/* Microchip Technology Inc. and its subsidiaries.  You may use this software 
+ * and any derivatives exclusively with Microchip products. 
+ * 
+ * THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS".  NO WARRANTIES, WHETHER 
+ * EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED 
+ * WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A 
+ * PARTICULAR PURPOSE, OR ITS INTERACTION WITH MICROCHIP PRODUCTS, COMBINATION 
+ * WITH ANY OTHER PRODUCTS, OR USE IN ANY APPLICATION. 
+ *
+ * IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+ * INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
+ * WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS 
+ * BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE.  TO THE 
+ * FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS 
+ * IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF 
+ * ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *
+ * MICROCHIP PROVIDES THIS SOFTWARE CONDITIONALLY UPON YOUR ACCEPTANCE OF THESE 
+ * TERMS. 
+ */
+
+/* 
+ * File:   
+ * Author: 
+ * Comments:
+ * Revision history: 
+ */
+
+// This is a guard condition so that contents of this file are not included
+// more than once.  
+#ifndef ST7735_H
+#define	ST7735_H
+#include <xc.h> // include processor files - each processor file is guarded.  
+#include "Fonts.h"
+//COMMANDS
+#define SWRESET 0x01//Software reset
+#define SLPIN   0x10//Sleep in and booster off
+#define SLPOUT  0x11//Sleep out and booster on
+#define PTLON   0x12//Partial mode on
+#define NORON   0x13//Partial mode off normal mode on
+#define INVOFF  0x20//display inversion off
+#define INVON   0x21//display inversion on
+#define GAMSET  0x26//Gamma curve select
+#define DISPOFF 0x28//Display off
+#define DISPON  0x29//Display on
+#define CASET   0x2A//set column address
+#define RASET   0x2B//set row address
+#define RAMWR   0x2C//memory write
+#define RAMRD   0x2E//memory read
+#define PTLAR   0x30//Partial start/end address
+#define TE0FF   0x34//tearing efect line off
+#define TEON    0x35//tearing efect line on
+#define IDMOFF  0x38//Idle mode off
+#define IDMON   0x39//Idle mode on
+#define COLMOD  0x3A//interface pixel format
+#define MADCTL  0x36//memoty acces control
+
+
+
+
+//FUNCTIONS
+#define FRMCTR1 0xB1
+#define FRMCTR2 0xB2
+#define FRMCTR3 0xB3
+#define INVCTR  0xB4//Display inversion control
+#define DISSET5 0xB6
+#define GMCTRP1 0xE0
+#define GMCTRN1 0xE1
+#define PWCTR1  0xC0//Power control for gamma settings
+#define PWCTR2  0xC1
+#define PWCTR3  0xC2//Power control in normal mode
+#define PWCTR4  0xC3//Power control in idle mode 8 color
+#define PWCTR5  0xC4//Power control in partial mode full colors
+#define VMCTR1  0xC5
+#define PWCTR6  0xFC
+
+
+//16bits Colors
+#define Red_Color 0xF800
+#define Green_Color 0xF81F
+#define Blue_Color 0xFFE0
+#define Black_Color 0x0000
+#define White_Color 0xFFFF
+
+#include <stdint.h>
+
+char image_high;
+char image_low;
+int color_img;
+int *const test[] = {0xFF, 0x81, 0x81, 0x81, 0xFF};
+uint8_t i;
+uint8_t j;
+uint8_t line_char;
+
+void ST7735S_80_x_160_init() {
+    
+    
+    write_command(SWRESET);//software reset
+     __delay_ms(150);
+    write_command(SLPOUT);//sleep mode out
+     __delay_ms(255);    
+
+    
+    write_command(FRMCTR1);//Frame rate control in normal mode
+    write_data(0x01);
+    write_data(0x2C);
+    write_data(0x2D);    
+    
+    write_command(FRMCTR2);//Frame rate control in idle mode
+    write_data(0x01);
+    write_data(0x2C);
+    write_data(0x2D);
+    
+    write_command(FRMCTR3);//Frame rate control in Partial mode
+    write_data(0x01);
+    write_data(0x2C);
+    write_data(0x2D);
+    write_data(0x01);
+    write_data(0x2C);
+    write_data(0x2D);
+    
+    write_command(INVCTR);//Display inversion control
+    write_data(0x07);//Frame version in all modes
+      
+    write_command(PWCTR1);//Power control 1
+    
+     write_data(0x02);//4.7V
+       write_data(0x02);//1.5uA
+       write_data(0x84);//Auto mode
+    
+      write_command(PWCTR2);
+      write_data(0xC5);//VGH VGL
+      
+         write_command(PWCTR3);
+      write_data(0x0A);//
+       write_data(0x00);//
+       
+       write_command(PWCTR4);
+      write_data(0x8A);//
+       write_data(0x2A);//
+               
+       write_command(PWCTR5);
+      write_data(0x8A);//
+       write_data(0xEE);//
+      
+      
+      write_command(VMCTR1);
+      write_data(0x0E);//
+      
+      write_command(INVOFF);//
+      
+      write_command(COLMOD);//RGB pixel format   
+      write_data(0x05);//16bit pixel
+      
+      
+      
+      write_command(MADCTL);//Memory access control
+      write_data(0b01011000);//RGB mode       
+      
+
+      
+      
+     
+      __delay_ms(10);
+      write_command(GMCTRP1);//RGB pixel format 
+       write_data(0x02);write_data(0x1C);write_data(0x07);write_data(0x12);
+       write_data(0x37);write_data(0x32);write_data(0x29);write_data(0x2D);
+       write_data(0x29);write_data(0x25);write_data(0x2B);write_data(0x39);
+       write_data(0x00);write_data(0x01);write_data(0x03);write_data(0x10);
+       
+       
+       write_command(GMCTRN1);//RGB pixel format 
+       write_data(0x03);write_data(0x1D);write_data(0x07);write_data(0x06);
+       write_data(0x2E);write_data(0x2C);write_data(0x29);write_data(0x2D);
+       write_data(0x2E);write_data(0x2E);write_data(0x37);write_data(0x3F);
+       write_data(0x00);write_data(0x00);write_data(0x02);write_data(0x10);
+       write_command(NORON);//
+        __delay_ms(10);
+       write_command(DISPON);//
+        __delay_ms(150);
+       
+       
+    
+}
+
+void ST7735S_Fill_rect()
+{
+    
+     
+   int q;
+int s;
+ 
+       
+       
+  write_command(CASET);
+  write_data(0);
+  write_data(25);//ST7735S column start in address 25
+  write_data(0);
+  write_data(127);
+  write_command(RASET);
+  write_data(0);
+  write_data(1);
+  write_data(0);
+  write_data(160);
+  
+   
+
+  write_command(RAMWR); // Write to RAM
+   CCS = 0;
+    DCs = 1; 
+    for( q =0; q<=80; q++)
+  {
+      for(s=0; s<=160; s++)
+      {
+   write_color(0xF0);  
+  write_color(0xF0); 
+  
+  }
+    
+}
+}
+
+void ST7735S_Print_Char(int color, char C_char)
+{
+       
+    i=0;
+    j=0;
+    
+    color_img = ~color;      
+  write_command(CASET);
+  write_data(0);
+  write_data(26);
+  write_data(0);
+  write_data(34);
+  write_command(RASET);
+  write_data(0);
+  write_data(1);
+  write_data(0);
+  write_data(5);
+  
+   
+
+  write_command(RAMWR); // Write to RAM
+   CCS = 0;
+    DCs = 1; 
+ for(i=0; i<=4; i++)
+  {
+      line_char = font[C_char][i];
+    
+       
+      for(j=0; j<=8; j++ )
+      {
+          
+          if(line_char & 1)
+          {     color_img = ~color;
+                 write_color(color_img >> 8);  
+                write_color(color_img & 0xFF); 
+          }
+          else
+          {
+              
+             color_img = ~Black_Color;
+             write_color(color_img >> 8);  
+                write_color(color_img & 0xFF); 
+          }
+          line_char>>=1;
+       
+                  
+   
+
+  }
+    
+    
+}
+}
+
+void ST7735S_Fill_display(int color)
+{
+   
+    i=0;
+    j=0;
+
+        
+  color_img = ~color;      
+  write_command(CASET);
+  write_data(0);
+  write_data(25);//ST7735S column start in address 25
+  write_data(0);
+  write_data(127);
+  write_command(RASET);
+  write_data(0);
+  write_data(1);
+  write_data(0);
+  write_data(160);
+  
+   
+
+  write_command(RAMWR); // Write to RAM
+   CCS = 0;
+    DCs = 1; 
+    for(i=0; i<=128; i++)
+  {
+      for(j=0; j<=160; j++)
+      {
+   write_color(color_img >> 8);  
+  write_color(color_img & 0xFF);
+
+  }
+    
+    
+}
+ 
+  /*
+  while(j>=50);
+  {
+   write_color(0Xff);  
+   write_color(0Xff);
+   j--;
+  }
+  CCS = 1;
+ */
+
+
+        
+
+}
+    
+
+
+void ST7735S_Fill_image(int Image_arr[])
+{
+    
+    int j = 0;
+
+        
+   
+
+        
+      
+  write_command(CASET);
+  write_data(0);
+  write_data(0);
+  write_data(0);
+  write_data(128);
+  write_command(RASET);
+  write_data(0);
+  write_data(0);
+  write_data(0);
+  write_data(160);
+  
+   
+
+  write_command(RAMWR); // Write to RAM
+   CCS = 0;
+    DCs = 1; 
+  for(int i =0; i<=20000; i++)
+  {
+     color_img = ~ Image_arr[i];
+   write_color(color_img >> 8);  
+  write_color(color_img & 0xFF);
+  }
+}
+  
+
+   
+   
+
+
+
+
+
+#endif	/* XC_HEADER_TEMPLATE_H */
+
